951cf772d334c5392453e0567080f76cf5e65e31
Gabriel Erdelyi <erdgabios@gmail.com>
2016-10-22 14:53:37 +0200

    UI upper half OK

---
+++ weather/WeatherVC.swift
@@ -0,0 +1,18 @@
+//
+//  WeatherVC.swift
+//  weather
+//
+//  Created by G on 21/10/2016.
+//  Copyright © 2016 erdgabios. All rights reserved.
+//
+
+import UIKit
+
+class WeatherVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    
+    }
+}
