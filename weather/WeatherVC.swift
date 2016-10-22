f9abe412dd6d499b58856b99545d8223ebd795d9
Gabriel Erdelyi <erdgabios@gmail.com>
2016-10-22 16:00:26 +0200

    Lect 90 Done

--- weather/WeatherVC.swift
+++ weather/WeatherVC.swift
@@ -9,6 +9,15 @@
 import UIKit
 
 class WeatherVC: UIViewController {
+    
+    @IBOutlet weak var dateLabel: UILabel!
+    @IBOutlet weak var currentTempLabel: UILabel!
+    @IBOutlet weak var locationLabel: UILabel!
+    @IBOutlet weak var currentWeatherImage: UIImageView!
+    @IBOutlet weak var currentWeatherTypeLabel: UILabel!
+    @IBOutlet weak var tableView: UITableView!
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
